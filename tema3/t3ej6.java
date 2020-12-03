3c3739c078800f7a94416b78a1b1d290161a71f6
giodurane <giovanny.dur@gmail.com>
2020-12-03 11:24:12 +0100

    actualizacion

--- tema3/t3ej6.java
+++ tema3/t3ej6.java
@@ -3,20 +3,8 @@
  *
  * Copyright 2020 Giovanny Duran <mario.duran.alu@iescampanillas.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ * Ejercicio 6
+ * Escribe un programa que calcule el área de un triángulo.
  *
  *
  */
@@ -26,13 +14,14 @@ public class t3ej6 {
 
 	public static void main (String[] args) {
     Scanner s = new Scanner (System.in);
-    System.out.print("Vamos a calcular el área de un triángulo. Introduzca los valores a continuación para efectuar la operación.");
-    System.out.println("Introduzca la altura del seno: ");
-    double num1 =
-    System.out.println("Introduzca la altura del coseno: ");
-    double num2 =
-    double area =
-    System.out.println("El valor del área del triángulo es: " + area + "Gracias por participar. Fin");
+    System.out.println("Vamos a calcular el área de un triángulo.");
+    System.out.println("Introduzca los valores a continuación para efectuar la operación.");
+    System.out.print("Introduzca la altura del seno: ");
+    double num1 = s.nextDouble();
+    System.out.print("Introduzca la altura del coseno: ");
+    double num2 = s.nextDouble();
+    double area = (num1 * num2)/2;
+    System.out.println("El valor del área del triángulo es: " + area + "m2. Gracias por participar. Fin");
 	}
 }
 
