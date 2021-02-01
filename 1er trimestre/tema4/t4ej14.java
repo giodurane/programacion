af046cdf5a0380fdb044f6f566f1e71355c3d477
Mario Giovanny Duran Eyzaguirre <mario.duran.alu@iescampanillas.com>
2021-02-01 12:13:44 +0100

    update

---
+++ 1er trimestre/tema4/t4ej14.java
@@ -0,0 +1,33 @@
+/*
+ * t4ej14.java
+ * 
+ * Copyright 2021 Mario Giovanny Duran Eyzaguirre <mario.duran.alu@iescampanillas.com>
+ * 
+ * Ejercicio 14
+ * Realiza un programa que diga si un número introducido 
+ * por teclado es par y/o divisible entre 5.
+ * 
+ */
+
+import java.util.Scanner;
+public class t4ej14 {
+	
+	public static void main (String[] args) {
+		Scanner s = new Scanner(System.in);
+    int numero;
+    System.out.print("Introduce un numero y te dire si es par o divisible entre 5: ");
+    numero = s.nextInt();
+    System.out.println("El numero introducido es: "+numero);
+    if ((numero % 2) == 0){
+      System.out.println("el numero introducido es par.");
+      } else {
+          System.out.println("es impar");
+        }
+    if((numero % 5) == 0){
+        System.out.println("el numero es divisible entre 5");
+      } else {
+          System.out.println("no es divisible entre 5");
+        }
+	}
+}
+
---
+++ 1er trimestre/tema4/t4ej16.java
@@ -0,0 +1,155 @@
+/*
+ * t4ej16.java
+ * 
+ * Copyright 2021 Mario Giovanny Duran Eyzaguirre <mario.duran.alu@iescampanillas.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * 
+ */
+
+import java.util.Scanner;
+public class t4ej16 {
+	
+	public static void main (String[] args) {
+    Scanner s = new Scanner(System.in);
+		int respuesta;
+    int verdadero =0;
+    int falso=0;
+
+    System.out.println("Test de Fidelidad. PREGUNTAS");
+    System.out.println("A. Tu pareja parece estar mas inquieta de lo normal sin motivo aparente?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+    
+    System.out.println("B. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      
+      System.out.println("C. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      
+      System.out.println("D. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      
+      
+      System.out.println("E. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      
+      System.out.println("F. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      System.out.println("G. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      } 
+      System.out.println("H. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      
+      System.out.println("I. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+      
+      System.out.println("J. Tu pareja ha aumentado su gasto en vestuario?");
+    System.out.println("responde con (1) Verdadero o (2)falso");
+    respuesta = s.nextInt();
+    if((respuesta==1) || (respuesta ==2)){
+    if (respuesta == 1){
+      verdadero++;
+      } else {
+        falso++;
+        }
+    } else{
+        System.out.println("lo siento, no es una respuesta correcta.");
+      }
+    /*
+    System.out.println("Ha sumado "+verdadero+" respuestas verdaderas, y "+falso+" respuestas falsas");
+    
+    rfinal*1;
+    System.out.print("hola hola "+rfinal+" sho ");*/
+	}
+}
+
