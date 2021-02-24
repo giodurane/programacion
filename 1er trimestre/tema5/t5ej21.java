92c26a6148cf29328ec7d6ca8d4b8dbed0b97e37
Mario Giovanny Duran Eyzaguirre <mario.duran.alu@iescampanillas.com>
2021-02-24 19:59:50 +0100

    F

--- 1er trimestre/tema5/t5ej21.java
+++ 1er trimestre/tema5/t5ej21.java
@@ -18,25 +18,28 @@ public class t5ej21 {
 	
 	public static void main (String[] args) {
 		Scanner s = new Scanner(System.in);
-    int entrada, pares, impar, mayorPares, contador;
-    double mediaImpar;
+    int entrada, z=0, pares, impar=0, mayorPares=0, contador=0;
+    double mediaImpar=0;
     
     do {
-    System.out.println("Introduzca un numero entero: ");
+    System.out.print("Introduzca un numero entero: ");
     entrada = s.nextInt();
     contador++;
     
-    if ((entrada %2)==1){
+      if ((entrada %2)==1){
         entrada = impar;
+        int aux=0;
+        z++;
+        double suma = aux + impar;
+        mediaImpar =  suma /z;
       } else {
-          
+          System.out.println("El mayor numero de los pares es: "+mayorPares+" fin.");
         }
-  
-      
-    } while(entrada <0);
-    System.out.println("Ha introducido: "+contador+" numeros");
-    System.out.println("La media de los numeros impares es: "+mediaInpar+" fin");
-    System.out.println("El mayor numero de los pares es: "+mayorPares+" fin.");
+      System.out.println("Ha introducido: "+contador+" numeros");
+      System.out.println("La media de los numeros impares es: "+mediaImpar+" fin");
+      System.out.println("El mayor numero de los pares es: "+mayorPares+" fin.");
+      } while(entrada <0);
+        System.out.println("Ha salido correctamente del programa");
 	}
 }
 
